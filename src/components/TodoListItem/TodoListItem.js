ae5370d68873b5f5d3bd49af0a677df4f2f4dbf0
Roma Kamenivskiy <jumji19992412@gmail.com>
2019-09-26 23:57:49 +0300

    added label onClick done

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,30 +1,53 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import "./TodoListItem.css";
 
-const TodoListItem = ({ value, important }) => {
-  const itemStyle = {
-    color: important ? "#1B9A65" : "black",
-    fontWeight: important ? "bold" : "normal"
+class TodoListItem extends Component {
+  state = {
+    done: false
   };
-  return (
-    <Fragment>
-      <li className="list-group-item todo-list-item" style={itemStyle}>
-        <span className="todo-list-item-label">{value}</span>
-        <div>
-          <button type="button" className="btn btn-outline-danger">
-            <i className="fa fa-trash" aria-hidden="true"></i>
-          </button>
-          <button type="button" className="btn btn-outline-info ml-2">
-            <i className="fa fa-exclamation" aria-hidden="true"></i>
-          </button>
-        </div>
-      </li>
-    </Fragment>
-  );
-};
 
-TodoListItem.defaultProps = {
-  important: false
-};
+  handleLabelClick = () => {
+    this.setState({
+      done: !this.state.done
+    });
+  };
+
+  render() {
+    const { value, important } = this.props;
+    const { done } = this.state;
+
+    const itemStyle = {
+      color: important ? "#1B9A65" : "black",
+      fontWeight: important ? "bold" : "normal"
+    };
+    let classNames = "todo-list-item-label";
+
+    if (done) {
+      classNames += " done";
+    }
+
+    return (
+      <Fragment>
+        <li className="list-group-item todo-list-item" style={itemStyle}>
+          <span className={classNames} onClick={this.handleLabelClick}>
+            {value}
+          </span>
+          <div>
+            <button type="button" className="btn btn-outline-danger">
+              <i className="fa fa-trash" aria-hidden="true"></i>
+            </button>
+            <button type="button" className="btn btn-outline-info ml-2">
+              <i className="fa fa-exclamation" aria-hidden="true"></i>
+            </button>
+          </div>
+        </li>
+      </Fragment>
+    );
+  }
+
+  static defaultProps = {
+    important: false
+  };
+}
 
 export default TodoListItem;
